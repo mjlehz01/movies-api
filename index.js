6a602d6adadfc76e2338a47f79efca7a6a77fad3
Jesus Hernandez <mjlehz@gmail.com>
2020-09-14 12:06:38 -0500

    version0.1

--- index.js
+++ index.js
@@ -6,16 +6,20 @@ const moviesApi = require('./routes/movies.js');
 
 const {
   logErrors,
+  wrapErrors,
   errorHandler,
 } = require('./utils/middleware/errorHandlers.js');
-const errorHandlers = require('./utils/middleware/errorHandlers.js');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 //body parser
 app.use(express.json());
-
+//routes
 moviesApi(app);
-
+//catch 404
+app.use(notFoundHandler);
+//erros middleware
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, function () {
